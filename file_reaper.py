7bc3aaab0941e11ee45a860bbcf2994033d4d534
MATTHEW MORALES <mtthwmrls9@gmail.com>
2023-11-22 09:16:55 -0700

    Modify reap_files

--- file_reaper.py
+++ file_reaper.py
@@ -2,11 +2,13 @@ from datetime import datetime
 from pathlib import Path
 
 def reap_files(target_dir: str, cutoff: datetime):
+    """
+    Deletes any files or directories older than the cutoff datetime
+    """
     target = Path(target_dir)
     for file in target.iterdir():
         last_modified = datetime.fromtimestamp(file.stat().st_mtime)
-        dt = cutoff - last_modified
-        if dt.total_seconds() > 0:
+        if last_modified < cutoff:
             if file.is_dir():
                 reap_files(str(file.resolve()), cutoff)
                 is_empty = not any(file.iterdir())
